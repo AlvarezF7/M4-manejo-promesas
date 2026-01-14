7436afa23f3494840c2d6d3a8f04c78e8b97651a
AlvarezF7 <alvarez.vic.382@gmail.com>
2026-01-14 19:34:59 -0300

    ejercicio completado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,70 @@
+import data from './data.js'; 
+
+//1 promesa con setTimeout
+
+let promesa = new Promise((resolve,reject) => {
+
+console.log("Esperando respuesta");
+
+setTimeout( () => {
+    resolve(data[0]);
+},3000);
+       
+});
+
+console.log("Registranto promesa");
+
+promesa.then (resultado => {
+    console.log("Respuesta:", resultado);
+})
+.catch(error => {
+    console.error("Error:", error);
+});
+
+//2-------promesa que depende de segundos actuales
+let promesaSegundos = new Promise((resolve, reject) =>{
+    let segundos = new Date().getSeconds();
+    let horaActual = new Date().toLocaleTimeString();
+
+    if(segundos > 30){
+        if (segundos % 2 === 0) {
+            resolve("Hora actual:" + horaActual);
+        }else {
+            reject("Error: segundos mayores a 30 e impares");
+        }
+    } else {
+        if (segundos % 2 !== 0) {
+            resolve("Hora actual: " + horaActual);
+        }else{
+            reject("Error: segundos menores o iguales a 30 y pares");
+        }
+    }
+});
+
+promesaSegundos
+.then(res => console.log(res))
+.catch(err => console.log(err));
+
+//------3 promesa con proceso pesado
+let promesaPesada = new Promise((resolve, reject) => {
+  console.log("Proceso pesado iniciado");
+
+  // Bloque pesado (loop intensivo)
+  let inicio = Date.now();
+  while (Date.now() - inicio < 4000) {
+
+  }
+
+  resolve("Proceso pesado terminado");
+});
+
+promesaPesada.then(res => {
+  console.log(res);
+  console.log("Después del proceso pesado");
+});
 
 
-alert(" morireeeeeee")
+
+
+
+
---
+++ assets/js/data.js
@@ -0,0 +1,35 @@
+
+export default [
+  {
+    "id": "1",
+    "nombre": "Mario",
+    "apellido": "Ross.",
+    "edad": 55,
+    "profesión":"fontanero",
+    "lugar": "Mushroom Kingdom"
+  },
+  {
+    "id": "2",
+    "nombre": "Luiggi",
+    "apellido": "Ross.",
+    "edad": 56,
+    "profesion":"fontanero",
+    "lugar": "Mushroom Kingdom"
+  },
+  {
+    "id": "3",
+    "nombre": "Browser",
+    "apellido": "dragon",
+    "edad": "indefinida",
+    "profesion":" villano",
+    "lugar": "Mushroom Kingdom"
+  },
+   {
+    "id": "4",
+    "nombre": "Peach ",
+    "apellido": " Toadstool",
+    "edad": 20,
+    "profesion":"princesa",
+    "lugar": "Mushroom Kingdom"
+  },
+];
